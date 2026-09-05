a9e7c4ff99f8e7e07f0ca62b74ebc4e9b6135a26
Abd Elrahman Mohamed <146685930+Abdo1420@users.noreply.github.com>
2026-09-05 18:30:10 +0300

    Add project files.

---
+++ C#.Basics/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace C_.Basics
+{
+    internal class Book
+    {
+        public string Title;
+        public int Pages =0;
+        public override string ToString()
+        {
+            return $"Title= {Title}, Pages= {Pages}";
+        }
+    }
+}
---
+++ C#.Basics/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using static System.Console;
+namespace C_.Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Create a Book class with a Title (string) and Pages (int). Create a Book object and store it in a variable of type object. Print it.
+            Book Mybook = new Book { Title = "C#", Pages = 250 };
+            WriteLine(Mybook);
+            #endregion
+
+        }
+    }
+}
