e775086fd0ceff0dc7b78b9b0afcfb8f371a000e
Abd Elrahman Mohamed <146685930+Abdo1420@users.noreply.github.com>
2026-09-05 18:44:59 +0300

    4 Answer

--- C#.Basics/Program.cs
+++ C#.Basics/Program.cs
@@ -6,9 +6,18 @@ namespace C_.Basics
     {
         static void Main(string[] args)
         {
-            #region Look at the line below. Is it a compile-time error, a runtime error, or a logical error? Fix it.
-           // copmile time error becouse cant write string in int type
-            int Pages = 464;
+            #region Write code that divides 10 by 0 inside a try block, catches the exception, prints "Cannot divide by zero", and then prints "Done" in a finally block.
+            try
+            {
+                int num1 = 10;
+                int num2 = 0;
+                WriteLine(num1 / num2);
+            }
+            catch (Exception ex)
+            {
+                WriteLine("Cannot divide by zero");
+            }
+            finally { WriteLine("Done"); }
             #endregion
 
         }
