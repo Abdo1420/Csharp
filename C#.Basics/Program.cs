d839144e6921d60512ff0b92db62411414f91ed9
Abd Elrahman Mohamed <146685930+Abdo1420@users.noreply.github.com>
2026-09-05 19:11:34 +0300

    10 Answer

--- C#.Basics/Program.cs
+++ C#.Basics/Program.cs
@@ -6,11 +6,12 @@ namespace C_.Basics
     {
         static void Main(string[] args)
         {
-            #region Given int pages = 464;, convert it into a string using ToString() and print its type using GetType() to prove it's now a string.
-            int Pages = 464;
-            string Pagestext = Pages.ToString();
-            WriteLine(Pagestext.GetType());
-
+            #region Declare int copies = 100;. Box it into an object variable, then unbox it back into a new int variable, and print both.
+            int Copies = 100;
+            object opj = Copies;
+            WriteLine(opj);
+            int New_Copies=(int)opj;
+            WriteLine(New_Copies);
             #endregion
 
 
