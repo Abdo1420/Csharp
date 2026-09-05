d89d1f6ec1cd7fdf951dfa8280e106d0e996eeac
Abd Elrahman Mohamed <146685930+Abdo1420@users.noreply.github.com>
2026-09-05 18:40:16 +0300

    1st Answer

--- C#.Basics/Program.cs
+++ C#.Basics/Program.cs
@@ -9,7 +9,7 @@ namespace C_.Basics
             #region Create a Book class with a Title (string) and Pages (int). Create a Book object and store it in a variable of type object. Print it.
             Book Mybook = new Book { Title = "C#", Pages = 250 };
             WriteLine(Mybook);
-            #endregion
+            #endregion 
 
         }
     }
