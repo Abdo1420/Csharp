7f7efd7ad3694cd6cb89ba9d991ff17ed95af292
Abd Elrahman Mohamed <146685930+Abdo1420@users.noreply.github.com>
2026-09-05 18:49:35 +0300

    8 Answer

--- C#.Basics/Program.cs
+++ C#.Basics/Program.cs
@@ -6,11 +6,18 @@ namespace C_.Basics
     {
         static void Main(string[] args)
         {
-            #region Given string pagesText = "464";, convert it into an int using the Convert class.
-            string Pagse = "464";
-            int Reale_Pagse = Convert.ToInt32(Pagse);
-            WriteLine(Reale_Pagse);
+            #region Given string yearText = "2023";, convert it using int.Parse(). Then given string badText = "abc";, use int.TryParse() to safely try converting it, and print "Invalid number" if it fails.
+            string yeartext = "2026";
+            int Year = int.Parse(yeartext);
+            WriteLine(yeartext);
+            string badText = "abc";
+            if (int.TryParse(badText, out int num))
+            {
+                WriteLine(num);
+            }
+            else { WriteLine("Invalid number"); }
             #endregion
+
 
         }
     }
