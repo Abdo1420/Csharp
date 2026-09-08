4fc2307810c9a924e0c18f7aea4146de3b35d3b8
Abd Elrahman Mohamed <146685930+Abdo1420@users.noreply.github.com>
2026-09-08 23:30:10 +0300

    answer 4

--- C#.Basics/Program.cs
+++ C#.Basics/Program.cs
@@ -10,11 +10,14 @@ namespace C_.Basics
     {
         static void Main(string[] args)
         {
-            #region Create a StringBuilder, Append() the text "Book List", then Append() " - Updated" onto the same object.Print the final result.
+            #region Using the StringBuilder from the question above, use Replace() to change "Book List" into "Library". Print the result.
             StringBuilder sb = new StringBuilder();
             sb.Append("Book List");
             sb.Append(" - Updated");
             WriteLine(sb);
+            sb.Replace("Book List", "Library");
+            WriteLine(sb);
+
             #endregion
 
         }
