e6f249daf44fc3a068922305c0168cdc5bc7b7f1
Abd Elrahman Mohamed <146685930+Abdo1420@users.noreply.github.com>
2026-09-08 23:41:49 +0300

    edit answer 5

--- C#.Basics/Program.cs
+++ C#.Basics/Program.cs
@@ -10,10 +10,10 @@ namespace C_.Basics
     {
         static void Main(string[] args)
         {
-            #region Build the same sentence as above, but using string interpolation ($"...").
+            #region Given string title = "Clean Code"; and int pages = 464;, build the sentence "Book: Clean Code,Pages: 464" using the + operator.() to change "Book List" into "Library". Print the result.
             string title = "Clean Code";
             int pages = 464;
-            WriteLine($"Book: {title}, Pages: {pages}");
+            WriteLine($"Book: {title}" + $"Pages: {pages}");
 
             #endregion
 
