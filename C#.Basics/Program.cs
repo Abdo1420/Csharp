8ccca699d325afc047ea4a14f259e73d682d71e8
Abd Elrahman Mohamed <146685930+Abdo1420@users.noreply.github.com>
2026-09-09 09:37:07 +0300

    answer 17

--- C#.Basics/Program.cs
+++ C#.Basics/Program.cs
@@ -10,12 +10,12 @@ namespace C_.Basics
     {
         static void Main(string[] args)
         {
-            #region Using the same books array, loop through it and print each title, but stop completely (break) once you reach "Refactoring".
+            #region Using the same books array, print every title except "The Pragmatic Programmer" (skip it with continue, don't stop the loop).
             string[] Books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
             for (int i = 0; i < Books.Length; i++)
             {
-                if (i < 2) { WriteLine(Books[i]); }
-                else { break; }
+                if (i ==1) { continue; }
+                else { WriteLine(Books[i]); }
             }
             #endregion
 
