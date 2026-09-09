976fd9f94d87d8fd3f22bdc734c15146dc6f0885
Abd Elrahman Mohamed <146685930+Abdo1420@users.noreply.github.com>
2026-09-09 08:38:41 +0300

    answer 9

--- C#.Basics/Program.cs
+++ C#.Basics/Program.cs
@@ -10,16 +10,14 @@ namespace C_.Basics
     {
         static void Main(string[] args)
         {
-            #region Given int pages = 464;, write an if / else statement that prints "Long Book" if pages is greater than 300, otherwise prints "Short Book".
+            #region Given int pages = 464; and bool isAvailable = true;, print "You can borrow this book" only if pages is  greater than 300 and isAvailable is true.Use the && operator. 
             int pages = 464;
-            if (pages > 300)
+            bool isAvailable = true;
+            if (isAvailable && pages > 300)
             {
-                WriteLine("Long Book");
+                WriteLine("You can borrow this book");
             }
-            else
-            {
-                WriteLine("Short Book");
-            }
+         
             #endregion
 
         }
