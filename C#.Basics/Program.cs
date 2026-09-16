ab3ebcf1d131f73557c8f2dc7562901db5966ed4
Abd Elrahman Mohamed <146685930+Abdo1420@users.noreply.github.com>
2026-09-16 16:35:55 +0300

    answer10

--- C#.Basics/Program.cs
+++ C#.Basics/Program.cs
@@ -8,29 +8,18 @@ namespace C_.Basics
     {
         static void Main(string[] args)
         {
-            #region Write a method bool TryGetPrice(string title, out double price) that returns true and sets price to 25.5 if title is "Clean Code", otherwise returns false and sets price to 0.Call it1and print the price if found
-            string title = "Clean Code";
-            double price = 0;
-            WriteLine(TryGetPrice(title, out price));
-            WriteLine(price); 
+            #region Write a method PrintBookInfo(string title, int pages = 300) where pages is optional. Call it once with only a title, and once passing both a title and pages.
+                        PrintBookInfo("Clean Code");
+            PrintBookInfo("C Sharp", 450); 
             #endregion
-        }
-        public static bool TryGetPrice(string title, out double price)
-        {
-            
-          if (title== "Clean Code" )
-            {
-                price = 25.5;
-                return true;
-            }
-            else
-            {
-                price = 0;
-                return false;
-            }
 
-           
-            
+        }
+        public static void PrintBookInfo(string title, int pages = 300)
+        {
+
+            WriteLine($"Book: {title}, Pages: {pages}");
+
+
         }
     }
 }
