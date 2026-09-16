a801fcd1928073a2d3519c8e6c57c1bd021d76a7
Abd Elrahman Mohamed <146685930+Abdo1420@users.noreply.github.com>
2026-09-16 10:13:21 +0300

    done

--- C#.Basics/Program.cs
+++ C#.Basics/Program.cs
@@ -6,9 +6,18 @@ namespace C_.Basics
     {
         static void Main(string[] args)
         {
-            #region Create a Book class with a Title (string) and Pages (int). Create a Book object and store it in a variable of type object. Print it.
-            Book Mybook = new Book { Title = "C#", Pages = 250 };
-            WriteLine(Mybook);
+            
+                if (Books.Length == 0)
+                {
+                    return;
+                }
+                else
+                {
+                    Write( Books[0]);
+                }
+            }
+            PrintFirstBook(Books);
+
             #endregion
 
         }
