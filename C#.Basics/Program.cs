5a95122887ae3cd5a053721e2de186c43863c706
Abd Elrahman Mohamed <146685930+Abdo1420@users.noreply.github.com>
2026-09-16 11:07:16 +0300

    answer2

--- C#.Basics/Program.cs
+++ C#.Basics/Program.cs
@@ -1,14 +1,15 @@
 ﻿
 using static System.Console;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace C_.Basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            #region Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print the second price(index 1).
-            double[] prices = { 25.5, 40.0, 33.75 };
-            WriteLine(prices[1]); 
+            #region Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
+            int[,] shelfCopies = new int[2, 2] { { 3, 5 }, { 1, 4 } };
+            WriteLine(shelfCopies[1, 0]); 
             #endregion
         }
     }
